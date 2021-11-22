a7a1980159d3fd2db6953d7902bf61b1d951d262
Ben Schultze <ben_schultze@outlook.com>
2021-11-22 14:57:40 -0500

    upload

--- Exercise Files/01_03/map_get_begin.js
+++ Exercise Files/01_03/map_get_begin.js
@@ -5,9 +5,14 @@
 
 const saturday = new Map();
 
-saturday.set(8, 'walk the dogs');
-saturday.set(12, 'lunch');
-saturday.set(3, 'watch a movie');
+saturday.set(8, "walk the dogs");
+saturday.set(12, "lunch");
+saturday.set(3, "watch a movie");
 
 //How do we get the value associated with the 12 key?
+const noon = saturday.get(12);
+console.log("What am I doing at 12pm on Saturday", noon);
+
 //How do we get the value associated with the 9 key?
+const nine = saturday.get(9);
+console.log(nine);

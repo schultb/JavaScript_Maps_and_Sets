5018139c895c3abf49b71e251be563cb233847b8
Ben Schultze <ben_schultze@outlook.com>
2021-11-22 14:31:56 -0500

    upload

--- Exercise Files/01_02/map_set_begin.js
+++ Exercise Files/01_02/map_set_begin.js
@@ -4,3 +4,11 @@
 //3pm: Watch a movie
 
 //Create a new Map() and add each to-do as a key-value pair.
+
+const saturday = new Map();
+
+saturday.set(8, "Walk the dogs");
+saturday.set(12, "Lunch");
+saturday.set(3, "Watch a movie");
+
+console.log(saturday);

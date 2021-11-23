773f64d6258cc11c4dd70e6c290e83632cd17fd2
Ben Schultze <ben_schultze@outlook.com>
2021-11-23 11:15:14 -0500

    upload

--- Exercise Files/01_07/map_keys_values_begin.js
+++ Exercise Files/01_07/map_keys_values_begin.js
@@ -5,9 +5,9 @@
 
 const saturday = new Map();
 
-saturday.set(8, 'walk the dogs');
-saturday.set(12, 'lunch');
-saturday.set(3, 'watch a movie');
+saturday.set(8, "walk the dogs");
+saturday.set(12, "lunch");
+saturday.set(3, "watch a movie");
 
 //What is the first key in your saturday Map object?
 //What is the second value in your saturday Map object?

cbf3816809a75a3e08a881e41bc6625143adb15c
Ben Schultze <ben_schultze@outlook.com>
2021-11-23 11:02:37 -0500

    upload

--- Exercise Files/01_05/map_size_begin.js
+++ Exercise Files/01_05/map_size_begin.js
@@ -5,8 +5,10 @@
 
 const saturday = new Map();
 
-saturday.set(8, 'walk the dogs');
-saturday.set(12, 'lunch');
-saturday.set(3, 'watch a movie');
+saturday.set(8, "walk the dogs");
+saturday.set(12, "lunch");
+saturday.set(3, "watch a movie");
 
 //What is the size of our saturday Map?
+const saturdaySize = saturday.size;
+console.log(saturdaySize);

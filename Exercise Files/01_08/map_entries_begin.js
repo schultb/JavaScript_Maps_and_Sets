6b8b329d7822f5d567a6c6692f744cb8c470384f
Ben Schultze <ben_schultze@outlook.com>
2021-11-29 13:31:18 -0500

    upload

--- Exercise Files/01_08/map_entries_begin.js
+++ Exercise Files/01_08/map_entries_begin.js
@@ -5,8 +5,15 @@
 
 const saturday = new Map();
 
-saturday.set(8, 'walk the dogs');
-saturday.set(12, 'lunch');
-saturday.set(3, 'watch a movie');
+saturday.set(8, "walk the dogs");
+saturday.set(12, "lunch");
+saturday.set(3, "watch a movie");
 
 //What is the third set of entries in your saturday Map object?
+const saturdayEntries = saturday.entries();
+saturdayEntries.next();
+saturdayEntries.next();
+
+const thirdEntries = saturdayEntries.next().value;
+
+console.log("What is our third entry in our map?", thirdEntries);
